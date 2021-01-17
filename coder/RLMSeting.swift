6f646c369fbf35bc2d24661c7985114f29a47fe0
manhnn <manhnn@phugroup.com>
2021-01-17 22:37:11 +0700

    M1

---
+++ coder/RLMSeting.swift
@@ -0,0 +1,31 @@
+//
+//  RLMColor.swift
+//  AppToDo
+//
+//  Created by Nguyen Manh on 15/01/2021.
+//
+ahihihi
+import Foundation
+import RealmSwift
+
+class RLMSetting: Object {
+    
+    @objc dynamic var idViewController = ""
+    @objc dynamic var colorRed = Float(0.0)
+    @objc dynamic var colorGreen = Float(0.0)
+    @objc dynamic var colorBlue = Float(0.0)
+    @objc dynamic var alpha = Float(0.0)
+    @objc dynamic var imagePath = ""
+    @objc dynamic var status = ""
+    
+    convenience init(idViewController: String, colorRed: Float, colorGreen: Float, colorBlue: Float, alpha: Float, imageName: String, status: String) {
+        self.init()
+        self.idViewController = idViewController
+        self.colorRed = colorRed
+        self.colorGreen = colorGreen
+        self.colorBlue = colorBlue
+        self.alpha = alpha
+        self.imagePath = imageName
+        self.status = status
+    }
+}
